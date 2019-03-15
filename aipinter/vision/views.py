4cb8715f163bced370f884c3ed7780457d40739b
toufanialfarisi <toufani.deeplearning@gmail.com>
2019-03-15 11:28:54 +0800

    fix some errors and structures

--- aipinter/vision/views.py
+++ aipinter/vision/views.py
@@ -1,7 +1,7 @@
 from aipinter.models import ImageFile
-from aipinter.vision.vision_handler import vision_utility
+from aipinter.vision.vision_handler import vision_utility, allowed_file, pred_processing
 from aipinter.vision.forms import ComputerVisionForm
-from flask_login import current_user
+from flask_login import current_user, login_required
 from flask import Blueprint, render_template, url_for, redirect, request, flash
 from aipinter import app, ALLOWED_EXTENSIONS, secure_filename
 import os
@@ -22,34 +22,9 @@ def vision_list():
     container = ImageFile.query.all()
     return render_template('vision_list.html', container=container)
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-def pred_processing(model, x_test, class_line=0.5):
-    x_test = np.expand_dims(x_test, axis = 0)
-    pred_result = model.predict(x_test)
-    result = []
-    for pred in pred_result:
-        if pred < class_line:
-            pred = 0
-            result.append(pred)
-        else:
-            pred = 1
-            result.append(pred)
-    # result = np.asarray(result, dtype=np.int32)
-    to_category = []
-    for cat in result:
-        if cat is 0:
-            to_cat = 'Negatif Retak'
-            to_category.append(cat)
-        else:
-            to_cat = 'Positive Retak'
-            to_category.append(to_cat)
-    return result, to_category
 
 @vision.route('/cvision', methods=['POST', 'GET'])
+@login_required
 def cvision():
     form = ComputerVisionForm()
     if request.method == 'POST':
@@ -72,11 +47,11 @@ def cvision():
 
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            a = os.path.join(os.getcwd() + '/aipinter/static/image_vision', filename)
-            file.save(a)
+            image_path = os.path.join(os.getcwd() + '/aipinter/static/image_vision', filename)
+            file.save(image_path)
             # flash('upload success', 'success')
 
-            image = filename
+            image = image_path
             model = file2.filename
             model_dir = os.path.join(os.getcwd() +'/aipinter/static/image_model', model)
             file2.save(model_dir)
@@ -84,17 +59,18 @@ def cvision():
             image_size = form.imagesize.data
             with graph.as_default():
                 model = load_model(model_dir)
-                # img = np.array(image)
+                # img_ = np.array(image)
                 img = cv2.imread(image)
                 img_ = cv2.resize(img, (image_size, image_size))
                 pred_val, pred_class = pred_processing(model, img_)
                 pred_val = pred_val[0]
                 pred_class = pred_class[0]
                         
-            user = ImageFile(image_file=a, description=form.description.data, prediction_val=pred_val, prediction_class=pred_class)
+            user = ImageFile(image_file=image_path, description=form.description.data, prediction_val=pred_val, prediction_class=pred_class)
             db.session.add(user)
             db.session.commit()
             flash('Dideteksi = ' + str(pred_class) + ' , confidence = '+str(pred_val * 100)+' %', 'success')
+            return render_template('cvision.html', form=form, file_url=image_path, filename=filename, os=os)
             # except TypeError:
                 # flash('predict result = ' + str(form.description.data), 'success')
             
--- aipinter/vision/vision_handler.py
+++ aipinter/vision/vision_handler.py
@@ -1,6 +1,9 @@
 import os
+import numpy as np
 from PIL import Image
 from flask import render_template, redirect, url_for
+from aipinter import app, ALLOWED_EXTENSIONS, secure_filename
+
 
 def vision_utility(image_file, model_file):
     image_filename = image_file.filename
@@ -15,6 +18,32 @@ def vision_utility(image_file, model_file):
     return image_data, path_model, path_image
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def pred_processing(model, x_test, class_line=0.5):
+    x_test = np.expand_dims(x_test, axis = 0)
+    pred_result = model.predict(x_test)
+    result = []
+    for pred in pred_result:
+        if pred < class_line:
+            pred = 0
+            result.append(pred)
+        else:
+            pred = 1
+            result.append(pred)
+    # result = np.asarray(result, dtype=np.int32)
+    to_category = []
+    for cat in result:
+        if cat is 0:
+            to_cat = 'Negatif Retak'
+            to_category.append(cat)
+        else:
+            to_cat = 'Positive Retak'
+            to_category.append(to_cat)
+    return result, to_category
 
     
 
